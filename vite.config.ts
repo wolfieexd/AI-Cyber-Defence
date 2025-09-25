9fbef425463031f966d266d2cc7df8ecb1fe33fd
Sujan <sujan5149481@gmail.com>
2025-09-26 02:23:53 +0530

    Prepare for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/AI-Cyber-Defence/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
